c52968ae29abfa36cd2939f3633ed98e61e133a1
Kento Norita <kento.norita@goodpatch.com>
2024-08-30 02:09:50 +0900

    Initial Commit

---
+++ src/components/button/index.tsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        solid: "shadow-sm",
+        outline: "",
+        ghost: "",
+      },
+      size: {
+        sm: "h-8 px-2.5 py-1 rounded-md",
+        md: "h-10 px-3 py-2",
+        lg: "h-12 px-3.5 py-2.5 rounded-md",
+      },
+      theme: {
+        primary: "bg-primary text-primary-foreground hover:bg-primary/90",
+        secondary:
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        negative:
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+      },
+    },
+    defaultVariants: {
+      variant: "solid",
+      size: "md",
+      theme: "primary",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, theme, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, theme, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
