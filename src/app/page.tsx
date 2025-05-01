c675f69e2b2c753993ce7da0ac419018dc5d93a7
touyou <fujiyou1121@gmail.com>
2025-05-01 10:33:41 +0900

    🐛 カンマいらなかった

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
       "command": "npx",
       "args": ["-y", "shadcn@canary", "registry:mcp"],
       "env": {
-        "REGISTRY_URL": "https://${hostname}/r/sparkle-style.json",
+        "REGISTRY_URL": "https://${hostname}/r/sparkle-style.json"
       }
     }
   }
