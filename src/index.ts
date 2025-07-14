1c415de905c63877b47f3665a87a7ea3a53cc730
touyou <contact@touyou.dev>
2025-07-14 15:14:53 +0900

    ➕ index.tsにButtonコンポーネントのエクスポートを追加
    - Buttonコンポーネントをindex.tsからエクスポートするように変更しました。

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./lib/utils";
 export * from "./components/ui/badge";
+export * from "./components/ui/button";
 export * from "./components/ui/card";
 export * from "./components/ui/checkbox";
 export * from "./components/ui/dialog";
